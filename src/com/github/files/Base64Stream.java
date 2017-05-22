16e4ad6be28699ae767bd9c2ffa796c0592f9dfb
yashwanth.m <yashwanth.m@Development-45.infotreesolutions.com>
2017-05-22 15:46:19 +0530

    Initial Code Move

---
+++ src/com/github/files/Base64Stream.java
@@ -0,0 +1,100 @@
+package com.github.files;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * Servlet implementation class FilesStream
+ * 
+ * EX: http://way2java.com/servlets/sending-image-client-servlet-example/
+ * 
+ * Test URL « localhsot:8080/SteamingServlet/files/base64?userField=image
+ * @author yashwanth.m
+ */
+@WebServlet(urlPatterns = { "/files/base64" }) /*asyncSupported = true,*/
+public class Base64Stream extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * https://www.w3schools.com/html/html5_video.asp
+	 * https://www.w3schools.com/html/mov_bbb.mp4
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		String userName = req.getParameter("userField");
+		System.out.println("UserName :"+userName);
+		if( userName != null & !userName.equals("") ) {
+			
+			/*To send binary data in a MIME body response, use the ServletOutputStream returned by getOutputStream.*/
+			ServletOutputStream sos = res.getOutputStream();// for binary stream of file
+			File f = null;
+			if( userName.equalsIgnoreCase("IMAGE") ) {
+				f = new File("D:\\SVN_Commit.PNG");
+				res.setContentType("image/png"); // see different MIME type
+			} else if( userName.equalsIgnoreCase("VIDEO") ) {
+				f = new File("D:\\mov_bbb.mp4");
+				res.setContentType("video/mp4"); // see different MIME type
+			}
+			
+			if( f != null ) {
+				DataInputStream dis = new DataInputStream(new FileInputStream(f));
+				byte[] byteArray = new byte[(int) f.length()];
+				
+				try {
+					dis.readFully(byteArray); // now the array contains the image
+				} catch (Exception e) {
+					byteArray = null;
+				} finally {
+					dis.close();
+				}
+				sos.write(byteArray); // send the byte array to client
+			}
+			sos.close();
+		} else {
+			res.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = res.getWriter();
+			File f = null;
+			String html = null;
+			if( userName.equalsIgnoreCase("IMAGE") ) {
+				f = new File("D:\\SVN_Commit.PNG");
+				html = "<img src='data:image/png;base64,{{-}}'>";
+				
+			} else {
+				f = new File("D:\\mov_bbb.mp4");
+				html = "<video controls><source type='video/mp4' src='data:video/mp4;base64,{{-}}' /></video>";
+			}
+			
+			if( f != null ) {
+				DataInputStream dis = new DataInputStream(new FileInputStream(f));
+				byte[] byteArray = new byte[(int) f.length()];
+				
+				try {
+					dis.readFully(byteArray); // now the array contains the image
+				} catch (Exception e) {
+					byteArray = null;
+				} finally {
+					dis.close();
+				}
+				String encodedfile = 
+						new String(org.apache.commons.codec.binary.Base64.encodeBase64( byteArray ), "UTF-8");
+				
+				html = html.replace("{{-}}", encodedfile);
+				writer.append(html);
+			}
+		}
+	}
+}
---
+++ src/com/github/files/URLStream.java
@@ -0,0 +1,93 @@
+package com.github.files;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class URLStream
+ * 
+ * Test URL « localhsot:8080/SteamingServlet/files/URLStream
+ */
+@WebServlet("/files/URLStream")
+public class URLStream extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public URLStream() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		File source = new File("D:\\SVN_Commit.PNG");
+		long start = System.nanoTime();
+		ServletOutputStream sos = response.getOutputStream();
+		
+		InputStream image = null;
+		BufferedInputStream bin = null;
+		BufferedOutputStream bout = null;
+		
+		try {
+			image = new FileInputStream(source);
+			bin = new BufferedInputStream( image );
+			bout = new BufferedOutputStream( sos );
+			int ch =0; ;
+			while((ch=bin.read())!=-1) {
+				bout.write(ch);
+			}
+		} finally {
+			bin.close();
+			image.close();
+			bout.close();
+			sos.close();
+		}
+		System.out.println("Time taken by Stream Copy = "+(System.nanoTime()-start));
+	}
+	
+	/**
+	 * <B>Java Copy File – Stream</B>
+	 * 
+	 * <P>This is the conventional way of file copy in java, here we create two Files, source and destination.
+	 * Then we create InputStream from source and write it to destination file using OutputStream for java
+	 * copy file operation.</p>
+	 * 
+	 * @param source       « the input file
+	 * @param dest         « the output file
+	 * @throws IOException « if any file is unavailable then throws exception.
+	 */
+	void copyFileUsingStream(File source, File dest) throws IOException {
+		long start = System.nanoTime();
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			is = new FileInputStream(source);
+			os = new FileOutputStream(dest);
+			byte[] buffer = new byte[1024];
+			int length;
+			while ((length = is.read(buffer)) > 0) {
+				os.write(buffer, 0, length);
+			}
+		} finally {
+			is.close();
+			os.close();
+		}
+		System.out.println("Time taken by Stream Copy = "+(System.nanoTime()-start));
+	}
+}
